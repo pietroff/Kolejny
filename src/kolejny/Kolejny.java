aea389551912cd626d2c76cb7d1992cfded07ee2
Piotr Nowicki <pietroff@tlen.pl>
2016-07-15 19:57:42 +0200

    First

--- src/kolejny/Kolejny.java
+++ src/kolejny/Kolejny.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kolejny;
 
 /**
@@ -11,9 +6,6 @@ package kolejny;
  */
 public class Kolejny {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
        System.out.println("ok");
     }
